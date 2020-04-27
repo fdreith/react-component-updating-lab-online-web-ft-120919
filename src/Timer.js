2dcef7441633048df822ab395a7b492fa831d6b2
Francesca Dreith <francescadreith@Francescas-MacBook-Air.local>
2020-04-27 11:30:29 -0700

    Done.

--- src/Timer.js
+++ src/Timer.js
@@ -11,6 +11,19 @@ class Timer extends Component {
   }
 
   //Your code here
+  // change the font color randomly everytime the component fires
+  componentDidUpdate() {
+    this.timer.current.style.color =
+      "#" + Math.floor(Math.random() * 16777215).toString(16);
+  }
+
+  //the only time we really need to update is when this.state.time changes.
+  shouldComponentUpdate(nextProps, nextState) {
+    if (this.state.time === nextState.time) {
+      return false
+    }
+    return true
+  }
 
   componentDidMount() {
     this.interval = setInterval(
